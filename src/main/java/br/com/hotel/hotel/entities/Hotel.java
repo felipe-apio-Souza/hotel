0f913453ba28d84d23e9dc8f0cf65ab3c72763e4
Felipe Apio de Souza <felipeapiodesouza@hotmail.com>
2022-06-10 21:01:44 -0300

    Entidade Hotel Mapeamento

---
+++ src/main/java/br/com/hotel/hotel/entities/Hotel.java
@@ -0,0 +1,43 @@
+package br.com.hotel.hotel.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Hotel")
+public class Hotel {
+    @Id
+    private String cnpj;
+    @Column(nullable = false)
+    private String endereco;
+
+    
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Hotel(String cnpj, String endereco) {
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+    }
+
+    public Hotel() {
+    }
+
+    
+}
