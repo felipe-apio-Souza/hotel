a20d34653ff1b134ff7685c794f7b8085ee61db6
felipe-apio-Souza <felipeapiodesouza@hotmail.com>
2022-06-19 20:52:44 -0300

    Registro de administrador

--- src/main/java/br/com/hotel/hotel/controller/AdministradorController.java
+++ src/main/java/br/com/hotel/hotel/controller/AdministradorController.java
@@ -8,21 +8,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.hotel.hotel.entities.Administrador;
-import br.com.hotel.hotel.services.AdministradorService;
+import br.com.hotel.hotel.repositories.AdministradorRepositories;
 
 
 @Controller
 public class AdministradorController {
     
-    @Autowired private AdministradorService service;
+    @Autowired private AdministradorRepositories repo;
+    private String msg;
 
     @GetMapping("/administrador")
     public String administrador(){
         return "administrador";
     }
 
+    @GetMapping("failed_adm")
+    public String failedAdm(){
+        return "failed_adm";
+    }
+
     @GetMapping("/administrador/form_adm")
     public String administradorFormulario(Model model){
+        model.addAttribute("mensagem", msg);
         model.addAttribute("adm", new Administrador());
         model.addAttribute("pageTitle", "Adicionar Novo Administrador");
         return "form_adm";
@@ -30,7 +37,13 @@ public class AdministradorController {
 
    @PostMapping("/administrador/save")
     public String administradorSave(Administrador adm, RedirectAttributes ra){
-        service.save(adm);
+        for(Administrador a : repo.findAll()){
+            if(a.getId() >= 1){
+                msg = "Sistema já possui Administrador";
+                return "redirect:/administrador/form_adm";
+            }
+        }
+        repo.save(adm);
         ra.addFlashAttribute("message", "Administrador Registrado");
         return "redirect:/administrador";
     }
---
+++ src/main/java/br/com/hotel/hotel/controller/AppController.java
@@ -0,0 +1,14 @@
+package br.com.hotel.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
