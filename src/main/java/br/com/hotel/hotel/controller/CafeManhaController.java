c5488b95add452a6538e0fe50b381bafca9c2bd8
Gabriel-Christen <bielchristen@gmail.com>
2022-06-20 21:51:10 -0300

    cafemanha html e controller

--- src/main/java/br/com/hotel/hotel/controller/CafeManhaController.java
+++ src/main/java/br/com/hotel/hotel/controller/CafeManhaController.java
@@ -1,5 +1,18 @@
 package br.com.hotel.hotel.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import br.com.hotel.hotel.repositories.CafeManhaRepositories;
+
+
+
+    @Controller
 public class CafeManhaController {
+    @Autowired private CafeManhaRepositories repo;
     
+    @GetMapping("/funcionario/cafemanha")
+    public String quarto(Model model){
+        model.addAttribute("listCafe", repo.findAll());
+        return "quartos_adm";
+    }
 }
