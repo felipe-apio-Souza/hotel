435252e09e631229450eae7f947852f8de55590f
marcelojuniiior <marcelojuniiior@gmail.com>
2022-06-20 21:45:18 -0300

    historico de custos e mapeamento incompleto

--- src/main/java/br/com/hotel/hotel/controller/HistoricoCustosController.java
+++ src/main/java/br/com/hotel/hotel/controller/HistoricoCustosController.java
@@ -1,5 +1,22 @@
 package br.com.hotel.hotel.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import br.com.hotel.hotel.entities.Hospede;
+import br.com.hotel.hotel.repositories.HistoricoCustosRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HistoricoCustosController {
-    
+    @Autowired private HistoricoCustosRepositories repo;
+
+
+    @GetMapping("/hospede/historico")
+    public String historico(Model model, Hospede hospede){
+        //model.addAttribute("listHistorico", repo.find(hospede.getId()));
+        return "historicoCustos";
+    }
+
 }
