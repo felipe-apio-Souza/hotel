19c377b745338feb7ca0ed912a97fccb45ff2d01
felipe-apio-Souza <felipeapiodesouza@hotmail.com>
2022-06-21 22:37:01 -0300

    listagem de candidatos

--- src/main/java/br/com/hotel/hotel/controller/FaleConoscoController.java
+++ src/main/java/br/com/hotel/hotel/controller/FaleConoscoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,5 +38,12 @@ public class FaleConoscoController {
         repo.save(candidato);
         ra.addFlashAttribute("message", "Candidato Cadastrado com sucesso");
         return "redirect:/faleConosco";
+    }
+
+    @GetMapping("/candidato/{candidatocpf}")
+    @ResponseBody
+    public byte[] exibirImagem(Model model, @PathVariable("candidatocpf") String candidatocpf){
+        Candidatos candidato = repo.getOne(candidatocpf);
+        return candidato.getCurriculo();
     }
 }
